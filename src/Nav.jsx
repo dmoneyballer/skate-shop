919eeca6d5c2f7e1e9c9621cc00aef736f5dbb2c
dmoneyballer <drew.gilliesbc@gmail.com>
2019-04-03 07:48:18 -0600

    moving to windows

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap'
+
 class Nav extends Component {
   render () {
     return (
-      <nav
-      className="navbar navbar-expand-lg navbar-light bg-light"
-      >I'm the Nav
-      <h1>yo yo yo How</h1></nav>
+      <nav className="container nav"
+      >
+      <ul id="navlinks">
+        <li>yo yo yo How</li>
+      </ul>
+      
+      </nav>
     );
   }
 }
