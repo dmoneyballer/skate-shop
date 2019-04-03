ab938bdec1ea813be730f8bda180e21f42862a26
drew <drew.gilliesbc@gmail.com>
2019-04-03 06:46:01 -0600

    first upload

---
+++ src/Nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap'
+class Nav extends Component {
+  render () {
+    return (
+      <nav
+      className="navbar navbar-expand-lg navbar-light bg-light"
+      >I'm the Nav
+      <h1>yo yo yo How</h1></nav>
+    );
+  }
+}
+
+export default Nav;
